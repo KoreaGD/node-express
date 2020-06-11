dfab7577c81fa8f191c1ad5425fe70db04d7e73d
Everton Trajano <evertontrajoli@gmail.com>
2020-06-10 22:59:01 -0300

    GET Users - Created

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+
+const userRoute = require('./routes/userRoute');
+
 const app = express();
 const port = 3000;
 
+userRoute(app);
+
 app.get('/', (req, res) => res.send('Olá mundo pelo Express!'))
 
-app.listen(port,() => console.log("API rodando na porta http://localhost:3000"));
+app.listen(port, () => console.log("API rodando na porta http://localhost:3000"));
 
